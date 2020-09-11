8464aecd887d27b0c057d9d7e26dae7910fe71d1
deyizhang <deyizhang2003@gmail.com>
2020-09-10 22:26:13 -0400

    Commit

--- Sereja and Dima (CF381-D2-A).cpp
+++ Sereja and Dima (CF381-D2-A).cpp
@@ -1,2 +1,13 @@
 #include <bits/stdc++.h> 
 using namespace std;
+
+int arraysize; int x; int y;
+
+
+int main(){
+cin>>arraysize;
+int arr[arraysize];
+for(int i = 0; i<arraysize;i++){
+    if(arr[-1])
+}
+}
