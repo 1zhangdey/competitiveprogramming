ecd93fb54b3cf5a971243dde93d31991966d8263
deyi zhang <deyizhang2003@gmail.com>
2020-10-20 03:13:02 +0000

    381A Sereja and Dima

--- Sereja and Dima (CF381-D2-A).cpp
+++ Sereja and Dima (CF381-D2-A).cpp
@@ -1,13 +1,39 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-int arraysize; int x; int y;
 
 
 int main(){
-cin>>arraysize;
-int arr[arraysize];
-for(int i = 0; i<arraysize;i++){
-    if(arr[-1])
+int lengthOfArray;
+cin>>lengthOfArray;
+int arr[lengthOfArray];
+
+for(int i = 0; i<lengthOfArray; i++){
+  cin>>arr[i];
 }
+
+int s_crawler = 0; 
+int d_crawler = lengthOfArray -1;
+int s_score = 0;
+int d_score = 0;
+int turn = 0;
+int max = 0;
+
+while(s_crawler<=d_crawler){
+  if(arr[s_crawler]>arr[d_crawler]){
+    max = arr[s_crawler];
+    s_crawler++;
+  } else{
+    max = arr[d_crawler];
+    d_crawler--;
+  }
+  if(turn%2==0){
+    d_score +=max;
+  }else{
+    s_score +=max;
+  }
+  turn++;
 }
+cout<<d_score<<" "<<s_score;
+}
+
