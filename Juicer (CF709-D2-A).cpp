719e526f19c46399ef39038105d9eda2ff7a9ebc
deyi zhang <deyizhang2003@gmail.com>
2021-01-16 03:22:10 +0000

    Commit

--- Juicer (CF709-D2-A).cpp
+++ Juicer (CF709-D2-A).cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main(){
+  
+int n,b,d,x;
+int count = 0; int waste = 0;
+cin>>n>>b>>d;
 
+for(int i = 0; i<n; i++){
+  cin>>x;
+  if(x<=b){
+    count += x;
+  }if(count>d){
+    waste+=1;
+    count = 0;
+  }
+}
+cout<<waste;
 }
