ee1d93fe0d0917c54a80e51405660bc6df95de02
deyi zhang <deyizhang2003@gmail.com>
2021-01-16 02:00:14 +0000

    Commit

---
+++ Shaass and Oskols (CF294-D2-A).cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main(){
+
+int n;int m;
+cin>>n;
+int arr [n];
+
+for(int i=0;i<n;i++){
+cin>>arr[i];
+}
+
+cin>>m;
+
+for(int i = 0;i<m;i++){
+  int x; int y; 
+  cin>>x>>y;
+  int right = arr[x-1] - y;
+  int left = y-1;
+  if(x-1>=1){
+		arr[x-2]+=left;
+	}if(x+1<=n){ 
+    arr[x] += right; 
+  }
+  arr[x-1] = 0;
+}
+
+for(int i=0;i<n;i++){
+		cout<<arr[i]<<endl;
+	}
+  return 0;
+}
