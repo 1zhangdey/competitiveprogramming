8944ce66862d914455ec106389d3ad9287ee18b2
deyizhang <deyizhang2003@gmail.com>
2021-02-14 15:59:18 -0500

    Commit

--- CCC '19 S1 - Flipper.cpp
+++ CCC '19 S1 - Flipper.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(){
-    int arr[4] = {1,2,3,4}, arrFinal[4];
-    string flipper;
+    int arr[4] = {1,2,3,4}, arrFinal[4] ={};
+    string flipper, stringSlice;
     cin>>flipper;
     for(int i = 0;i<flipper.length();i++){
-        if(reinterpret_cast<const char *>(char(flipper[i])) == "H"){
+        stringSlice = flipper[i];
+        if(stringSlice == "H"){
                 arrFinal[0] = arr[2];
                 arrFinal[1] = arr[3];
                 arrFinal[2] = arr[0];
@@ -16,9 +17,12 @@ int main(){
             arrFinal[1] = arr[0];
             arrFinal[2] = arr[3];
             arrFinal[3] = arr[2];
+
         }
-       copy(arr,arr+4,arrFinal);
+        memcpy(arr,arrFinal,sizeof arr);
+
     }
-    cout<<arrFinal[0]<<" "<< arrFinal[1]<<"\n"<<arrFinal[2]<<" "<< arrFinal[3];
+    cout<<arrFinal[0]<<" "<< arrFinal[1]<<"\n"<<arrFinal[2]<<" "<< arrFinal[3]<<"\n";
+
 
 }
